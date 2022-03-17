0c44aae035764b34a39791c5f5a7d80f41313bbe
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-03-17 22:00:44 +0530

    Time: 15 ms (32.20%), Space: 13.6 MB (85.34%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,16 @@
 class Solution {
 public:
-    int findfirst(vector<int>nums,int target)
-    {
-        int s=0,e=nums.size()-1,mid;
-        int res=-1;
-        while(s<=e)
-        {
-            mid=s+(e-s)/2; // to remove integer overflow
-            if(target==nums[mid])
-            {
-                res=mid;
-                e=mid-1; // instead of breaking search more occurence in first half
-            }
-            else if(nums[mid]>target)
-            {
-                e=mid-1;
-            }
-            else
-                s=mid+1;
-            
-        }
-        return res;
-    }
-    int findlast(vector<int>nums,int target)
-    {
-        int s=0,e=nums.size()-1,mid;
-        int res=-1;
-        while(s<=e)
-        {
-            mid=s+(e-s)/2; // to remove integer overflow
-            if(target==nums[mid])
-            {
-                res=mid;
-                s=mid+1; // instead of breaking search more occurence in second half
-            }
-            else if(nums[mid]>target)
-            {
-                e=mid-1;
-            }
-            else
-                s=mid+1;
-            
-        }
-        return res;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l=-1,r=-1;
-       l=findfirst(nums,target);
-        if(l!=-1)
-            r=findlast(nums,target);
-        return {l,r};
+        vector<int>pos;
+        if(!binary_search(nums.begin(),nums.end(),target))
+        {
+            pos.push_back(-1);
+            pos.push_back(-1);
+        }
+        else{
+        pos.push_back(lower_bound(nums.begin(),nums.end(),target)-nums.begin());
+        pos.push_back(upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1);
+        }
+        return pos;
     }
 };
