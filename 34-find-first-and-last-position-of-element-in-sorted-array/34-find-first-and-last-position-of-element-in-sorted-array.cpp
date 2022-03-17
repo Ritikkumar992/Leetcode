8c4bc95c58e34ad75a077ab86a0695801feb5ad1
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-03-17 21:59:35 +0530

    Time: 25 ms (6.47%), Space: 14.3 MB (5.36%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,54 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        // Time:O(N)
-        int l = -1, r = -1;
-        for(int i = 0;i<nums.size();i++)
+    int findfirst(vector<int>nums,int target)
+    {
+        int s=0,e=nums.size()-1,mid;
+        int res=-1;
+        while(s<=e)
         {
-            if(nums[i] == target)
+            mid=s+(e-s)/2; // to remove integer overflow
+            if(target==nums[mid])
             {
-                if(l == -1)
-                    l = i;
-                r = i;
+                res=mid;
+                e=mid-1; // instead of breaking search more occurence in first half
             }
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            else
+                s=mid+1;
+            
         }
+        return res;
+    }
+    int findlast(vector<int>nums,int target)
+    {
+        int s=0,e=nums.size()-1,mid;
+        int res=-1;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2; // to remove integer overflow
+            if(target==nums[mid])
+            {
+                res=mid;
+                s=mid+1; // instead of breaking search more occurence in second half
+            }
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            else
+                s=mid+1;
+            
+        }
+        return res;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l=-1,r=-1;
+       l=findfirst(nums,target);
+        if(l!=-1)
+            r=findlast(nums,target);
         return {l,r};
     }
 };
