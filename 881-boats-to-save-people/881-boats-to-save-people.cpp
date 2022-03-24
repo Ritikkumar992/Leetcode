7e40e9ef5c81d2fb78403680869abba20756cf08
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-03-24 21:32:26 +0530

    Time: 127 ms (32.08%), Space: 42 MB (10.43%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        // Greedy (Two-Pointer).
+        // Greedy (Two-Pointer). : Time : O(nlogn) and Space: O(N)
         sort(people.begin(),people.end());
         int i = 0, j = people.size()-1;
         int ans = 0;
