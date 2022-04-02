a09790a14975153fb4f8056639842bc0f7501470
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-04-02 17:08:14 +0530

    Time: 70 ms (48.86%), Space: 24.1 MB (18.74%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
-public:
-    bool isPalindrome(string s, int i, int j)
+bool checkPalindrome(string s, int i, int j)
+{
+    while(i<j)
     {
-         while (i < j) {
-            if (s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
+        if(s[i] != s[j])
+        {
+            return false;
         }
-        
-        return true;
+        i++;
+        j--;
     }
+    return true;
+}
 public:
     bool validPalindrome(string s) {
         int i = 0, j = s.size()-1;
-        while(i<j){
-            if(s[i] != s[j]){
-                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+        while(i<j)
+        {
+            if(s[i] != s[j])
+            {
+                return  checkPalindrome(s,i+1,j) || checkPalindrome(s,i,j-1);
             }
             i++;
             j--;
