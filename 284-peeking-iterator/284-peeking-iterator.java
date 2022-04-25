b99dd1d6b44bd03637487cb22fd4fc548ec00122
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-04-25 09:18:51 +0530

    Time: 6 ms (61.79%), Space: 43.6 MB (6.28%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -0,0 +1,30 @@
+class PeekingIterator implements Iterator<Integer> {
+    private final Iterator<Integer> iterator;
+    private boolean hasPeeked;
+    private Integer peekedElement;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    if(iterator==null)
+	        throw new NullPointerException();
+	    else
+	        this.iterator = iterator;
+	}
+
+	public Integer peek() {
+        peekedElement = hasPeeked?peekedElement:iterator.next();
+        hasPeeked = true;
+        return peekedElement;
+	}
+
+	@Override
+	public Integer next() {
+	    int nextElem = hasPeeked?peekedElement:iterator.next();
+        hasPeeked = false;
+        return nextElem;
+	}
+
+	@Override
+	public boolean hasNext() {
+	    return hasPeeked || iterator.hasNext();
+	}
+}
