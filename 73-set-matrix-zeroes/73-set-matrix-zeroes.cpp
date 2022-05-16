ccc0c52b1ab410a135bdb0833a062212e6b2b253
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-16 12:13:45 +0530

    Time: 27 ms (18.31%), Space: 13.4 MB (19.25%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         
-        vector<int>dummy1(row,-1), dummy2(col,-1);
+        vector<int> dummy1(row,-1), dummy2(col,-1);
         
         for(int i = 0;i<row;i++){
             for(int j = 0;j<col;j++){
@@ -15,7 +15,7 @@ public:
             }
         }
         for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
+            for(int j =0;j<col;j++){
                 if(dummy1[i] == 0 || dummy2[j] == 0)
                     matrix[i][j] = 0;
             }
