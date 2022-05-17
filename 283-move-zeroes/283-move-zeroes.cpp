1d500e0d1f6bb52eb62efad99116258f84187202
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-17 11:04:09 +0530

    Time: 34 ms (44.47%), Space: 20.6 MB (5.26%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //Apporach_01: without usign in-place:
-        vector<int> res;
-        for(auto it:nums)
-        {
-            if(it!= 0)
+        //Approach_02: Space-optimal:
+        int cnt0 = 0;
+        for(auto it:nums){
+            if(it == 0)
+                cnt0++;
+        }
+        // retain the original position of non-zero element;
+        vector<int>res;
+        for(auto it:nums){
+            if(it != 0)
                 res.push_back(it);
         }
-        for(auto it:nums)
-        {
-            if(it == 0)
-                res.push_back(it);
+        //move all zeroes to the end:
+        while(cnt0--){
+            res.push_back(0);
         }
         nums = res;
     }
