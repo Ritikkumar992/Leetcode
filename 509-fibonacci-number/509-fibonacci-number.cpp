cab51e563c547da0c2a5fb1475117d1f58ff0eee
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-17 18:20:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.94%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
+    int dp[31];
+    int helper(int n)
+    {
+        if(n<=1)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = helper(n-1)+helper(n-2);
+    }
 public:
     int fib(int n) {
-        if(n == 0 || n == 1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        //Recursion to Memoization : Time : O(n) and SPace: O(n)+O(n)
+        memset(dp,-1,sizeof(dp));
+        int ans = helper(n);
+        return ans;
     }
 };
