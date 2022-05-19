1a25892aea9ed19dac82986c63e6ca1690a0ef01
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-19 15:05:26 +0530

    Time: 20 ms (79.41%), Space: 19.6 MB (14.26%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //Approach_02 : Using HashMap Time:O(nlogn) and Space:O(n)
-        int n = nums.size();
-        map<int,int>mp;
-        for(auto it:nums){
-            mp[it]++;
+        //Moore's Voting Algorithm:
+        int cnt = 0; // for tacking the cnt of element:
+        int element = 0; // for which element we are counting:
+        
+        for(auto it:nums)
+        {
+            if(cnt == 0){
+                element = it;
+            }
+            if(element == it)
+                cnt += 1;
+            else
+                cnt -= 1;
         }
-        for(auto it:mp){
-            if(it.second > n/2)
-                return it.first;
-        }
-        return -1;
+        return element;
     }
 };
