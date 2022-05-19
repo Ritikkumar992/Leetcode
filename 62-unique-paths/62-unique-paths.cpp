b178fc8379d239e6262941348121ad36d31a216f
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-19 21:01:41 +0530

    Time: 3 ms (39.23%), Space: 5.9 MB (74.41%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,12 @@
 class Solution {
-    int countUniquePaths(int i , int j, int m, int n,vector<vector<int>> &dp)
-    {
-        if(i == (m-1) && j == (n-1))
-            return 1;
-        if(i>=m || j>=n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        else
-            return dp[i][j] =  countUniquePaths(i+1,j,m,n,dp)+countUniquePaths(i,j+1,m,n,dp);
-    }
 public:
     int uniquePaths(int m, int n) {
-        //Using recursion to memoization : TIme:O(n*m) and Space:O(N*M);
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return countUniquePaths(0,0,m,n,dp);
+            int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
     }
 };
