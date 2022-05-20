759e24990462e0264cc0322811042b8caeb62c65
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-20 14:42:47 +0530

    Time: 25 ms (47.32%), Space: 12.1 MB (11.37%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Using HashSet: Time: O(n)
-        vector<int>res;
-        unordered_map<int,int> mp;
-        
-        for(int i = 0;i<nums.size();i++)
+        //Using HashSet: Time: O(n) and Space:O(n)
+        vector<int> res;
+        unordered_map<int,int>mp;
+        for(int i =0;i<nums.size();i++)
         {
-            if(mp.find(target-nums[i]) != mp.end())
-            {
-                res.emplace_back(mp[target-nums[i]]);
-                res.emplace_back(i);
-                return res;
+            if(mp.find(target-nums[i]) != mp.end()){
+                res.push_back(i);
+                res.push_back(mp[target-nums[i]]);
             }
-            else{
-                mp[nums[i]] = i;
-            }
+            mp[nums[i]] = i;
         }
         return res;
     }
