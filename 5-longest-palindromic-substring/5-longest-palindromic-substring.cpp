e6ecf2aec63d2832d333b65ab58cc6cff5ff79ad
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-23 16:31:14 +0530

    Time: 243 ms (44.31%), Space: 25.7 MB (40.65%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,45 @@
-class Solution {
+class Solution
+{
 public:
-    string longestPalindrome(string s) {
-        if(s.size()==0) return "";
-        int i = 0, j = 0;
+    string longestPalindrome(string s)
+    {
         int n = s.size();
-        //initialize P[n][n], we only need half of P, and initialize it like: (e.g. : s="abbc")
-        vector<vector<bool>> P(n, vector<bool> (n, false));
-        for(int x = 0;x<n;x++){
-            P[x][x]=true;
-            if(x==n-1) break;
-            P[x][x+1] = (s[x]==s[x+1]);
-        }
-        //dp 
-        for(int i = n-3; i>=0; --i){
-            for(int j = i+2;j<n; ++j){
-                P[i][j] = (P[i+1][j-1] && s[i]==s[j]);     
-            }
-        }
-        //get maxstr result
-        int max = 0;
-        string maxstr = "";
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(P[i][j]==true and j-i+1>max){
-                    max = j-i+1;
-                    maxstr = s.substr(i,j-i+1);
+        if (n == 0)
+            return "";
+
+        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
+        bool dp[n][n];
+
+        //Initialize with false
+
+        memset(dp, 0, sizeof(dp));
+
+        //Every Single character is palindrome
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        string ans = "";
+        ans += s[0];
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    //If it is of two character OR if its susbtring is palindrome.
+                    if (j - i == 1 || dp[i + 1][j - 1])
+                    {
+                        //Then it will also a palindrome substring
+                        dp[i][j] = true;
+
+                        //Check for Longest Palindrome substring
+                        if (ans.size() < j - i + 1)
+                            ans = s.substr(i, j - i + 1);
+                    }
                 }
             }
         }
-        return maxstr;
+        return ans;
     }
 };
