f91fc23ddd77c349347718f2d98bbbd00a0a99dd
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-24 10:10:35 +0530

    Time: 4 ms (73.76%), Space: 6.8 MB (88.01%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        //Using Stack: Time:O(n) and Space:O(n)
+        //Using Two Pointer Approach: Time:O(n) and Space:O(1)
         int maxLen = 0;
-        stack<int> st;
-        st.push(-1);
-        for(int i = 0;i<s.size();i++)
-        {
+        int left = 0, right = 0;
+        //traversal from left side to right side:
+        for(int i = 0;i<s.size();i++){
             if(s[i] == '(')
-                st.push(i);
+                left++;
             else
-            {
-                st.pop();
-                if(st.empty())
-                    st.push(i);
-                else
-                    maxLen = max(maxLen, i-st.top());
-            }
+                right++;
+            if(left == right)
+                maxLen = max(maxLen, right*2);
+            else if(right >= left)
+                left = 0, right = 0;
+        }
+        // reset the left and right pointer and traverse from right to left side:
+        left = 0, right = 0;
+        for(int i = s.size()-1;i>=0;i--){
+            if(s[i] == '(')
+                left++;
+            else
+                right++;
+            if(left == right)
+                maxLen = max(maxLen, 2*left);
+            else if(left >= right)
+                left = 0, right = 0;
         }
         return maxLen;
     }
