e743559f283cefdba1dd7998fef5599761fc6706
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-25 10:40:33 +0530

    Time: 794 ms (9.46%), Space: 87.8 MB (25.32%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int maxEnvelopes(vector<vector<int>>& E) {
-        sort(E.begin(), E.end(), [](vector<int>& a, vector<int>& b) 
-             -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
-        vector<int> dp;
-        for (auto& env : E) {
-            int height = env[1];
-            int left = lower_bound(dp.begin(), dp.end(), height) - dp.begin();
-            if (left == dp.size()) dp.push_back(height);
-            dp[left] = height;
+    static bool cmp(vector<int>& a,vector<int>& b){
+        if(a[0]==b[0])
+            return a[1]>b[1];
+        return a[0]<b[0];
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(),envelopes.end(),cmp);
+        vector<int>v;
+        for(auto i:envelopes){
+            auto it=lower_bound(v.begin(),v.end(),i[1]);
+            if(it==v.end())
+                v.push_back(i[1]);
+			//if the element is greater than all elements push it
+            else
+                *it=i[1];
+			//if it less than or equal to a particular element replace it,because it may form the Longest Increasing subsequence
+            
         }
-        return dp.size();
+        return v.size();
+
+        
     }
 };
