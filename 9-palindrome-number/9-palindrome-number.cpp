3877a9d08a41716b94f9f52913a7a1af3bda838b
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-26 15:40:28 +0530

    Time: 11 ms (86.74%), Space: 5.9 MB (75.47%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //Approach_01:  Time: O(n) and Space:O(2n)
-        string s = to_string(x);
-        string t = s;
-        reverse(t.begin(),t.end());
+        //Approach_03: Time:O(n) and Space:O(1)
+        if(x<0 || (x%10 == 0 && x!= 0))
+            return false;
         
-        return t==s;
+        int ans = 0;
+        while(x>ans)
+        {
+            int rem = x%10;
+            ans = ans*10 + rem;
+            x /= 10;
+        }
+        return ans == x || x == ans/10;
     }
 };
