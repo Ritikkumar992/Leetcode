b895792b896c0b3af86ad02ec328d17c7b16f502
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-26 19:16:30 +0530

    Time: 3 ms (38.51%), Space: 5.9 MB (84.68%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        //Brute force: Time:O(n) + O(logn) and Space: O(n) 
-        vector<int> res;
-        for(int i = 1;i<=n;i++)
+        //Better Approach : Time:O(n) and Space:O(1)
+        for(int i = 1;i<=n/2;i++)
         {
             if(n%i == 0)
-                res.push_back(i);
+                k--;
+            if(k == 0)
+                return i;
         }
-        sort(res.begin(),res.end());
-        if(res.size()<k)
-            return -1;
-        return res[k-1];
+        if(k-1 == 0)
+            return n;
+        return -1;
     }
 };
