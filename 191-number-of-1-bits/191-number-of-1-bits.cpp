451a15f778d98363bf673018b79a4aeb738e9b50
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-26 08:56:24 +0530

    Time: 7 ms (12.48%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        //Approach_02: no of iteration is equal to no of set bits 
-        int cnt = 0;// count the set bits
-        while(n>0) // iterate until all bits are traversed;
-        {
-            ++cnt;
-            n = n&(n-1); // change the first set bit from right to 0.
-        }
-        return cnt;
+        //Approach_03: Built in method:
+        return __builtin_popcount(n);
     }
 };
