7ebbdc8e4b5f07b5164d0123c6c35f12f596a059
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-27 19:54:57 +0530

    Time: 8 ms (30.72%), Space: 8.8 MB (50.76%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        //Approach_01:Linear Scan: Time:O(n) and Space:O(1)
         int n = nums.size();
         for(int i = 0;i<n-1;i++){
             if(nums[i] > nums[i+1])
