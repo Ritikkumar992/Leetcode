62e77a30a6ab42d3fd9e4cd281b6eb2bbfc176f0
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-27 14:37:41 +0530

    Time: 10 ms (19.26%), Space: 11.1 MB (30.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // Time : O(log n) and space:O(1)
-        int l =0, r = nums.size()-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(target == nums[mid])
-                return mid;
-            
-            // if there exists rotations; the middle element is the left part of the array.
-            if(nums[mid] > nums[r])
-            {
-                if(target < nums[mid] && target >= nums[l])
-                    r = mid-1;
-                else
-                    l = mid+1;
-            }
-            
-            // if there exists rotation; the middle element is the right part of the array.
-            else if(nums[mid] < nums[l])
-            {
-                if(target > nums[mid] && target <= nums[r])
-                    l = mid+1;
-                else
-                    r = mid-1;
-            }
-            else{
-                if(target < nums[mid])
-                    r = mid -1;
-                else
-                    l = mid + 1;
-            }
+        //Using Linear Search: Time:O(n) and Space:O(1)
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == target)
+                return i;
         }
         return -1;
     }
