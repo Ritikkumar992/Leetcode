28d65ca82eaf1db4aa26c8551332c940303d968d
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-27 14:26:52 +0530

    Time: 31 ms (69.03%), Space: 22.4 MB (55.76%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // Approach_03: Using XOr operation -> Time:O(n) and Space:O(1)
-        int ele = 0;
-        for(int i = 0;i<nums.size();i++){
-            ele = ele^nums[i];
-        }
-        return ele;
+        //Approach_04: Binary Search : Time:O(logn) and Space:O(1)
+        int n = nums.size();
+        int low = 0;
+        int high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid % 2 == 0) {
+                if (nums[mid] != nums[mid + 1]) 
+                    //Checking whether we are in right half
+                    high = mid - 1; //Shrinking the right half
+                    else
+                        low = mid + 1; //Shrinking the left half
+                } else {
+                    //Checking whether we are in right half
+                    if (nums[mid] == nums[mid + 1]) 
+                        high = mid - 1; //Shrinking the right half
+                    else
+                        low = mid + 1; //Shrinking the left half
+                }
+            }
+            return nums[low];
     }
 };
