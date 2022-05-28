47f4f03ae54e58b279f05bfebf632fbe49209795
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-28 10:24:34 +0530

    Time: 31 ms (36.74%), Space: 17.9 MB (93.84%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //Aproach_01: Maths approach:
+        //Using Binary Searc: time:O(nlogn) and Space:O(1)
         int n = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        int ans = n*(n+1)/2;
+        sort(nums.begin(),nums.end());
+        int start = 0, end = n, mid = (start+end)/2;
         
-        return ans - sum;
+        while(start < end)
+        {
+            mid = (start+end)/2;
+            if(nums[mid] > mid)
+                end = mid;
+            else
+                start = mid+1;
+        }
+        return start;
     }
 };
