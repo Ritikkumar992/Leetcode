b9b0bef17868c6f7d95fc9b976591896278bca4f
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-05-31 11:56:51 +0530

    Time: 8 ms (81.18%), Space: 7.6 MB (34.46%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,14 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        //Using HashSet: Time:O(n) and Space:O(n)
-        unordered_set<ListNode*> st;
-        while(head != NULL)
+        // Using Slow and Fast Pointer : Time:O(n) and Space:O(1)
+        if(head == NULL||head->next == NULL) 
+            return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* dummy = head;
+        while(fast->next != NULL && fast->next->next != NULL)
         {
-            if(st.find(head) != st.end())
-                return head;
-            st.insert(head);
-            head = head->next;
+            fast = fast->next->next;
+            slow = slow->next;
+            if(slow == fast){
+                while(dummy != slow){
+                    slow = slow->next;
+                    dummy = dummy->next;
+                }
+                return slow;
+            }
         }
         return NULL;
     }
