2edc7fbaa8896441924b7d0b2ce13f1956471ff9
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-01 10:10:38 +0530

    Time: 27 ms (96.63%), Space: 73.9 MB (13.09%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -10,33 +10,29 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //Using Extra Space: Time:O(n) and Space:O(n)
-        vector<int> num1, num2;
+     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> f1, f2;
+        ListNode *res = new ListNode(0);
+        int sum  = 0;
         while(l1){
-            num1.push_back(l1->val);
-            l1 = l1->next;
+            f1.push(l1->val);l1 = l1->next;
         }
         while(l2){
-            num2.push_back(l2->val);
-            l2 = l2->next;
+            f2.push(l2->val);l2 = l2->next;
         }
-        int m = num1.size(), n = num2.size();
-        int sum = 0, carry = 0;
-        
-        ListNode* head = NULL, *p = NULL;
-        for(int i = m-1, j = n-1;i>=0 || j>=0 || carry >0;i--,j--)
-        {
-            sum = carry;
-            if(i>=0)
-                sum += num1[i];
-            if(j>=0)
-                sum += num2[j];
-            carry = sum/10;
-            p = new ListNode(sum%10);
-            p->next = head;
-            head = p;
+
+        while(!f1.empty() || !f2.empty()){
+            if(!f1.empty()){
+                sum+=f1.top();f1.pop();
+            }
+            if(!f2.empty()){
+                sum+=f2.top();f2.pop();
+            }
+            res->val = sum % 10;
+            ListNode* head = new ListNode(sum /= 10);
+            head->next = res;
+            res = head;
         }
-        return head;
+        return res->val? res : res->next;
     }
 };
