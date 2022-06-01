24d041cbb93eda34969b932237ac3131622e75f2
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-01 08:41:13 +0530

    Time: 5 ms (40.23%), Space: 8.4 MB (94.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        //Using Prefix Sum method: Time:O(n) and Space:O(n)
+        //Using Input array as output: Time:O(n) and Space:O(1)
         int n = nums.size();
-        vector<int> prefix(n);
-        prefix[0] = nums[0];
-        
         for(int i = 1;i<n;i++){
-            prefix[i] = nums[i]+prefix[i-1];
+            nums[i] += nums[i-1];
         }
-        return prefix;
+        return nums;
     }
 };
