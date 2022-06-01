0420b0b531e0a819d52df4c9a3630495cbd497fb
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-01 09:27:29 +0530

    Time: 17 ms (24.39%), Space: 11.5 MB (17.99%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -13,22 +13,23 @@ public:
     }
 };
 */
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*, Node*> m;
-        int i=0;
+        //Using HashMap: Time:O(2n) and Space:O(n)
+        map<Node*, Node*> mp;
         Node* ptr = head;
-        while (ptr) {
-            m[ptr] =new Node(ptr->val);
+        while(ptr){
+            mp[ptr] = new Node(ptr->val);
             ptr = ptr->next;
         }
         ptr = head;
-        while (ptr) {
-            m[ptr]->next = m[ptr->next];
-            m[ptr]->random = m[ptr->random];
+        while(ptr){
+            mp[ptr]->next = mp[ptr->next];
+            mp[ptr]->random = mp[ptr->random];
             ptr = ptr->next;
         }
-        return m[head];
+        return mp[head];
     }
 };
