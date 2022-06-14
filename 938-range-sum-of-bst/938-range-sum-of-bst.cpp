c166554a0419d14ee94bafa2a7c155f45962b695
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-15 00:07:33 +0530

    Time: 185 ms (38.36%), Space: 64.7 MB (62.05%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -10,32 +10,19 @@
  * };
  */
 class Solution {
-void dfs(TreeNode* root, vector<int>& inorder)
-    {
-        if(root == NULL)
-            return;
-        dfs(root->left, inorder);
-        inorder.push_back(root->val);
-        dfs(root->right, inorder);
-    }
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        //Approach_01: Using Inorder traversal;
-        vector<int> inorder;
-        dfs(root,inorder);
-        int sum = 0;
-        int i = 0, j= 0;
-        for(i = 0;i<inorder.size();i++){
-            if(inorder[i] == low)
-                break;
-        }
-        for(j = 0;j<inorder.size();j++){
-            if(inorder[j] == high)
-                break;
-        }
-        for(int x = i;x<=j;x++){
-            sum += inorder[x];
-        }
-        return sum;
+        //Approach_02: Using Binary Search;
+        if(root == NULL)
+            return 0;
+        int ans = 0;
+        if(root->val >= low && root->val <= high)
+            ans += root->val;
+        if(root->val < high)
+            ans += rangeSumBST(root->right, low,high);
+        if(root->val > low)
+            ans += rangeSumBST(root->left, low, high);
+        
+        return ans;
     }
 };
