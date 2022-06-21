b14c019cd07dba37759a967b6b98ae6d3b5ce5a1
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-21 08:58:57 +0530

    Time: 163 ms (44.07%), Space: 53.6 MB (77.22%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        //Approach_01: Using Max_heap:
         int n = heights.size();
-        int totalBricksUsed = 0;
-        priority_queue<int> bricksUsed;
-        int reach = 0;
-        for (; reach < n-1; reach++) {
-            // If current height is higher, we don't have to use
-            // bricks or ladder.
-            if (heights[reach] >= heights[reach+1]) {
-                continue;
+        priority_queue<int> max_heap;
+        for(int i =0;i<n-1;i++)
+        {
+            int diff = heights[i+1]-heights[i];
+            if(diff > 0)
+                max_heap.push(-diff);
+            if(max_heap.size()>ladders){
+                bricks += max_heap.top();
+                max_heap.pop();
             }
-        
-            // We have to use either brick or ladder.
-            int diff = heights[reach+1] - heights[reach];
-        
-            if (totalBricksUsed + diff <= bricks) {
-                // Prefer bricks because we can later change from 
-                // bricks to ladder as we will see below.
-                totalBricksUsed += diff;
-                bricksUsed.push(diff);
-            } else if (ladders > 0) {
-                // If even bricks are insufficient, check ladders.
-                if (!bricksUsed.empty() && bricksUsed.top() > diff) {   // Alert: Corner case ** 
-                    totalBricksUsed = totalBricksUsed - bricksUsed.top() + diff;
-                    bricksUsed.pop();
-                    bricksUsed.push(diff); // Thanks to @amanda_86
-                }
-                ladders--;
-            } else {
-                // Unfortunately, it's not possible to go ahead any further.
-                break;
-            }
+            if(bricks < 0)
+                return i;
         }
-        return reach; // This is the max we can go ahead.
+        return n-1;
     }
 };
