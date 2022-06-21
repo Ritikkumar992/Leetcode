87b10505b104749e393f92d7dad16f0fe83ce61b
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-21 09:01:24 +0530

    Time: 197 ms (23.41%), Space: 53.6 MB (94.58%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -3,15 +3,15 @@ public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         //Approach_01: Using Max_heap:
         int n = heights.size();
-        priority_queue<int> max_heap;
+        priority_queue<int,vector<int>, greater<int>> min_heap;
         for(int i =0;i<n-1;i++)
         {
             int diff = heights[i+1]-heights[i];
             if(diff > 0)
-                max_heap.push(-diff);
-            if(max_heap.size()>ladders){
-                bricks += max_heap.top();
-                max_heap.pop();
+                min_heap.push(diff);
+            if(min_heap.size()>ladders){
+                bricks -= min_heap.top();
+                min_heap.pop();
             }
             if(bricks < 0)
                 return i;
