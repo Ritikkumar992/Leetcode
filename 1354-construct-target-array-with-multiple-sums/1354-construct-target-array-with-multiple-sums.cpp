fd30bc7a64d683cbd97f20f08550f98429b99f53
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-24 11:11:42 +0530

    Time: 81 ms (25.17%), Space: 30.1 MB (51.05%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-    auto s = accumulate(begin(target), end(target), (long long)0);
-    priority_queue<int> q(begin(target), end(target));
-    while (s > 1 && q.top() > s / 2) {
-        auto cur = q.top(); q.pop();
-        s -= cur;
-        if (s <= 1)
-            return s;
-        q.push(cur % s);
-        s += cur % s;
-    }
-        return s == target.size();
+        priority_queue<int> pq;
+        unsigned int sum = 0;
+        for (int num : target)
+            sum += num, pq.push(num);
+        while (pq.top() != 1) {
+            int num = pq.top();
+            pq.pop();
+            sum -= num;
+            if (num <= sum || sum < 1) return false;
+            num %= sum, sum += num, pq.push(num ? num : sum);
+        }
+        return true;
     }
 };
