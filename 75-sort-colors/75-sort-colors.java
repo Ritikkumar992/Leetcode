1c8845f6fd4ae720f444129bb8a7962416ac023d
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-24 09:28:06 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (89.91%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,6 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Approach_1: using sort method: Time:O(nlogn) and SpaceO(nlogn)
-        Arrays.sort(nums);
+        // Approach_1: using counting method: Time:O(n) and SpaceO(n)
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for(int num:nums){
+            if(num == 0)
+                cnt0++;
+            if(num == 1)
+                cnt1++;
+            if(num == 2)
+                cnt2++;
+        }
+        int i = 0;
+        while(cnt0!=0){
+            nums[i++] = 0;
+            cnt0--;
+        }
+        while(cnt1!=0){
+            nums[i++] = 1;
+            cnt1--;
+        }
+        while(cnt2 != 0){
+            nums[i++] = 2;
+            cnt2--;
+        }
     }
 }
