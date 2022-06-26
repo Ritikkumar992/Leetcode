5c47c57995b496293911f47065dd553e7cdd1679
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-26 20:53:20 +0530

    Time: 5 ms (40.74%), Space: 15.9 MB (26.06%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,24 +3,19 @@
 // Space complexity : O(N*(2^N))
 class Solution {
 public:
-    vector<vector<int>> ans;
-    
-    void sub(vector<int> &nums, int i, vector<int> temp)
-    {
-        if(i==nums.size())
-        {
+    void sub(vector<int> &nums, int i, vector<int> temp,vector<vector<int>> &ans){
+        if(i==nums.size()){
             ans.push_back(temp);
             return;
         }
-        
-        sub(nums, i+1, temp);
+        sub(nums, i+1, temp,ans);
         temp.push_back(nums[i]);
-        sub(nums, i+1, temp);
+        sub(nums, i+1, temp,ans);
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
         vector<int> temp;       
-        sub(nums, 0, temp);
+        sub(nums, 0, temp,ans);
         return ans;
     }
 };
