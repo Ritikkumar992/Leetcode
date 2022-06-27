9393687a70708f67b1bbc203a2ee9a7d2ee757d8
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-27 16:56:43 +0530

    Time: 115 ms (6.27%), Space: 45.7 MB (12.92%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,36 +1,60 @@
 class Solution {
-    //Time;O(n!*n) and Space:O(n)
     public:
-        void solve(int col, vector < string > & board, vector < vector < string >> & ans, vector < int > &          leftrow, vector < int > & upperDiagonal, vector < int > & lowerDiagonal, int n) {
-      if (col == n) {
-        ans.push_back(board);
-        return;
-      }
-      for (int row = 0; row < n; row++) {
-        if (leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
-          board[row][col] = 'Q';
-          leftrow[row] = 1;
-          lowerDiagonal[row + col] = 1;
-          upperDiagonal[n - 1 + col - row] = 1;
-          solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
-          board[row][col] = '.';
-          leftrow[row] = 0;
-          lowerDiagonal[row + col] = 0;
-          upperDiagonal[n - 1 + col - row] = 0;
+    bool isSafe(int row, int col, vector<string>board,int n){
+        //check upper diagonal;
+        int duprow = row,dupcol = col;
+        while(row>=0 && col>=0){
+            if(board[row][col] == 'Q')
+                return false;
+            row--;
+            col--;
         }
-      }
+        col = dupcol;
+        row = duprow;
+        // check for left col:
+        while(col>=0){
+            if(board[row][col] == 'Q')
+                return false;
+            col--;
+        }
+        col = dupcol;
+        row = duprow;
+        // check for lower diagonal:
+        while(row<n && col>=0){
+            if(board[row][col] == 'Q')
+                return false;
+            row++;
+            col--;
+        }
+        // if none of the condition satiesfies 
+        return true;
     }
-
-  public:
-    vector < vector < string >> solveNQueens(int n) {
-      vector < vector < string >> ans;
-      vector < string > board(n);
-      string s(n, '.');
-      for (int i = 0; i < n; i++) {
-        board[i] = s;
-      }
-      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
-      solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
-      return ans;
+    public:
+    void solve(int col, vector<string>&board,vector<vector<string>>&ans,int n){
+        //base case:
+        if(n == col){
+            ans.push_back(board);
+            return;
+        }
+        // for each column we are doing our main logic:
+        for(int row = 0;row<n;row++){
+            if(isSafe(row,col,board,n))
+            {
+                board[row][col] = 'Q';
+                solve(col+1,board,ans,n);
+                board[row][col] = '.';
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i] = s;
+        }
+        solve(0,board,ans,n);
+        return ans;
     }
 };
