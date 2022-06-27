68bf5047f630d388d39a76eb6dbffd632ba895d7
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-27 09:36:53 +0530

    Time: 6 ms (44.72%), Space: 7.7 MB (78.18%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,22 +1,31 @@
 class Solution {
-public:
-    void recurPermute(int index, vector<int>&nums, vector<vector<int>>&ans)
-    {
-        if(index == nums.size()){
-            ans.push_back(nums);
+    private:
+    void findPermutation(vector<int>& nums,vector<int>&ds, int freq[],vector<vector<int>> &ans){
+        // base case:
+        if(ds.size() == nums.size()){
+            ans.push_back(ds);
             return;
         }
-        for(int i = index;i<nums.size();i++)
-        {
-            swap(nums[index],nums[i]);
-            recurPermute(index+1, nums, ans);
-            swap(nums[index],nums[i]);
+        // main logic:
+        for(int i= 0;i<nums.size();i++){
+            if(!freq[i]){
+                freq[i] = 1;
+                ds.push_back(nums[i]);
+                findPermutation(nums,ds,freq,ans);
+                ds.pop_back();
+                freq[i] = 0;
+            }
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        // Approach_01: Time:n!*n and Space:O(n)+O(n)
         vector<vector<int>> ans;
-        recurPermute(0,nums,ans);
+        vector<int> ds;
+        int freq[nums.size()];
+        for(int i =0;i<nums.size();i++)
+            freq[i] = 0;
+        findPermutation(nums,ds,freq,ans);
         return ans;
     }
 };
