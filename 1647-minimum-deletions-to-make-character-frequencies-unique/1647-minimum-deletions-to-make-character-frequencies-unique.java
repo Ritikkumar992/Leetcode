36fe9dda26b7e250389a3b6e96387827f4fe0311
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-28 11:21:32 +0530

    Time: 16 ms (66.52%), Space: 54.4 MB (57.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,18 +1,26 @@
 class Solution {
     public int minDeletions(String s) {
-        int []freq = new int[26];
-        for(int i =0;i<s.length();i++){
-            freq[s.charAt(i) - 'a']++;
+        // Time:O(n+k^2logk) and Space:O(k)
+        int freq[] = new int[26];
+        for(int i = 0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
         }
-        
+        PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                max_heap.add(freq[i]);
+            }
+        }
         int cnt = 0;
-        HashSet<Integer> st = new HashSet<>();
-        for(int i= 0;i<26;i++){
-            while(freq[i]>0 && st.contains(freq[i])){
-                freq[i]--;
+        while(max_heap.size()>1)
+        {
+            int topElement = max_heap.remove();
+            if(topElement == max_heap.peek()){
+                if(topElement - 1 > 0){
+                    max_heap.add(topElement-1);
+                }
                 cnt++;
             }
-            st.add(freq[i]);
         }
         return cnt;
     }
