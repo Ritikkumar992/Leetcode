078033f8bbcbbf2075c55a9622afef5f3d53c900
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-28 10:44:45 +0530

    Time: 62 ms (81.03%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        // Time:  O(N + K^2 \log K)O(N+K^2logK) and Space:O(k)
-        // Step 1: Create a freq array and store the freq of each char
-        vector<int> freq(26,0);
-        for(int i =0;i<s.length();i++){
-            freq[s[i]-'a']++;
+        vector<int>freq(26,0);
+        for(char c:s){
+            freq[c-'a']++;
         }
-        // Step 2: Create a mac_heap and store the freq of each char
-        priority_queue<int> max_heap;
-        for(int i = 0;i<26;i++){
-            if(freq[i]>0)
-                max_heap.push(freq[i]);
-        }
-        // Step 3: Traverse through the max_heap and if top 2 elem are same increase the cnt 
-        //and push the topElement -1 each time
-        int deleteCnt = 0;
-        while(max_heap.size()>1)
+        int deletecnt = 0;
+        unordered_set<int>st;
+        for(int i = 0;i<26;i++)
         {
-            int topElement = max_heap.top();
-            max_heap.pop();
-            
-            if(topElement == max_heap.top()){
-                if(topElement - 1 > 0){
-                    max_heap.push(topElement-1);
-                }
-                deleteCnt++;
+            while(freq[i] && st.find(freq[i]) != st.end()){
+                freq[i]--;
+                deletecnt++;
             }
+            st.insert(freq[i]);
         }
-        return deleteCnt;
+        return deletecnt;
     }
 };
