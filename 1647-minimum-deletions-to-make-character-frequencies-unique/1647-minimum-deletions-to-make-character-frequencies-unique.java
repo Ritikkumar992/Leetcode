00c617eb14153e5c3b4b0fc6549b2f0466d1a4c6
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-28 10:57:51 +0530

    Time: 10 ms (95.62%), Space: 42.9 MB (94.55%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,31 +1,19 @@
 class Solution {
     public int minDeletions(String s) {
-        // Time:  O(N + K^2 \log K)O(N+K^2logK) and Space:O(k)
-        // Step 1: Create a freq array and store the freq of each char
         int []freq = new int[26];
         for(int i =0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
+            freq[s.charAt(i) - 'a']++;
         }
-        // Step 2: Create a mac_heap and store the freq of each char
-        PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0;i<26;i++){
-            if(freq[i]>0)
-                max_heap.add(freq[i]);
-        }
-        // Step 3: Traverse through the max_heap and if top 2 elem are same increase the cnt 
-        //and push the topElement -1 each time
-        int deleteCnt = 0;
-        while(max_heap.size()>1)
-        {
-            int topElement = max_heap.remove();
-            
-            if(topElement == max_heap.peek()){
-                if(topElement - 1 > 0){
-                    max_heap.add(topElement-1);
-                }
-                deleteCnt++;
+        
+        int cnt = 0;
+        HashSet<Integer> st = new HashSet<>();
+        for(int i= 0;i<26;i++){
+            while(freq[i]>0 && st.contains(freq[i])){
+                freq[i]--;
+                cnt++;
             }
+            st.add(freq[i]);
         }
-        return deleteCnt;
+        return cnt;
     }
 }
