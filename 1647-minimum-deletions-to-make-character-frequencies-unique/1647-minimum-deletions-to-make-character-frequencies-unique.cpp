00ec048c50bca05f3caf08d0f7be3d02adcee14d
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-06-28 11:12:59 +0530

    Time: 66 ms (75.21%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int minDeletions(string s) {
+        //Approach_02: using priority_queue:
         vector<int>freq(26,0);
         for(char c:s){
             freq[c-'a']++;
         }
-        int deletecnt = 0;
-        unordered_set<int>st;
-        for(int i = 0;i<26;i++)
-        {
-            while(freq[i] && st.find(freq[i]) != st.end()){
-                freq[i]--;
-                deletecnt++;
+        priority_queue<int> max_heap;
+        for (int i = 0; i<26; i++){
+            if (freq[i] > 0) {
+                max_heap.push(freq[i]);
             }
-            st.insert(freq[i]);
         }
-        return deletecnt;
+        int cnt = 0;
+        while(max_heap.size()>1)
+        {
+            int top1 = max_heap.top();
+            max_heap.pop();
+            if(top1 == max_heap.top()){
+                if(top1-1 > 0){
+                    max_heap.push(top1-1);
+                }
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
