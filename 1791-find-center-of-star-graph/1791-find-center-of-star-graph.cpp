2c0386a3c0a3fb1b2f0643313d2d8c13ba805f53
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-01 21:27:55 +0530

    Time: 514 ms (6.53%), Space: 81.4 MB (25.26%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
+        //Approach_02: Using Adjacency Matrix:
         int n = edges.size();
-        int ans1 = edges[0][0];
-        int ans2 = edges[0][1];
-        
-        for(int i = 1;i<n;i++){
-            if(edges[i][0] == ans1 || edges[i][1] == ans1){
-                return ans1;
-            }
-            else if(edges[i][0] == ans2 || edges[i][1] == ans2){
-                return ans2;
-            }
+        vector<int> adj[n+2];
+        for(int i =0;i<n;i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
-        return -1;
+        for(int i=1;i<=n+1;i++){
+            if(adj[i].size() == n)
+                return i;
+        }
+        return 0;
     }
 };
