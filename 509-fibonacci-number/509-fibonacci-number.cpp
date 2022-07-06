0a422765342a79bddfe8a3d678eacf44e666d925
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-06 08:33:27 +0530

    Time: 12 ms (37.67%), Space: 5.8 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        //Space Optimization:
-        //Recursion to Tabulation : Time : O(n) and Space:O(1)
-        if(n<=1)
+        if(n==0 || n ==1)
             return n;
-        int prev2 = 0, prev1 = 1;
-        for(int i = 2;i<=n;i++){
-            int cur = prev2 + prev1;
-            prev2 = prev1;
-            prev1 = cur;
-        }
-        return prev1;
+        return fib(n-1)+fib(n-2);
     }
 };
