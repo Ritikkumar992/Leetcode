30b823a2e50ddf3231acaf05f7408c91d198e1c0
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-21 10:45:37 +0530

    Time: 100 ms (85.70%), Space: 42.6 MB (38.24%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int n = arr.size();
-        
-        int prefix[n];
-        
-        prefix[0] = arr[0];
-        
-        for(int i = 1; i < n; i++)
-        {
-            prefix[i] = arr[i] + prefix[i - 1];
-        }
-        
-        unordered_map<int,int> mp;
-        
+    int subarraySum(vector<int>& nums, int k) {
+        // Better Appraoch: Time:O(n) and Space:O(1)
+        int n  = nums.size();
         int ans = 0;
-        
-        for(int i = 0; i < n; i++)
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            prefix[i] = nums[i]+prefix[i-1];
+        }
+        unordered_map<int,int> mp;
+        for(int i = 0;i<n;i++)
         {
             if(prefix[i] == k)
                 ans++;
-            if(mp.find(prefix[i] - k) != mp.end())
-            {
+            if(mp.find(prefix[i]-k) != mp.end()){
                 ans += mp[prefix[i] - k];
             }
-            
             mp[prefix[i]]++;
         }
         return ans;
