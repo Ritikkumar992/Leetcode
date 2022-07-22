c5d04c580cea476d5256e42db6df856233eb6ec9
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-22 10:10:16 +0530

    Time: 2765 ms (5.00%), Space: 51.1 MB (7.00%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,15 +1,20 @@
 class Solution {
+    vector<int>dp;
+    bool solve(int i , vector<int>&nums){
+        if(i == nums.size()-1)
+            return true;
+        if(dp[i] != -1)
+            return dp[i];
+        for(int k = i+1;k<=i+nums[i];k++){
+            if(solve(k,nums))
+                return dp[i] = true;
+        }
+        return dp[i] = false;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        //Time:O(n) and Space:O(1)
-        int n = nums.size();
-        int reachable = 0;
-        
-        for(int i = 0;i<n;i++){
-            if(reachable < i)
-                return false;
-            reachable = max(reachable, i+nums[i]);
-        }
-        return true;
+        //Recursion -> Memoization:
+        dp.assign(nums.size()-1, -1);
+        return solve(0,nums);
     }
 };
