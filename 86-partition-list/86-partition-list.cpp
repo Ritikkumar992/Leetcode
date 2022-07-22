8147666deac28b7faf42466fd67c762667ddda4a
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-22 08:34:08 +0530

    Time: 4 ms (87.24%), Space: 10.3 MB (73.43%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,28 +11,30 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* small = new ListNode(0);
-        ListNode* higher = new ListNode(0);
+        // Approach_01: Using two pointer:
+        // step_01: Creating two dummy LIstNode for storing our smaller and higher value:
+        ListNode* small = new ListNode(-1);
+        ListNode* high = new ListNode(-1);
         
+        //step_02: creating two pointer to traverse in small and high listNode:
         ListNode* smallHead = small;
-        ListNode* highHead = higher;
+        ListNode* highHead = high;
         
+        //step_03: Traverse throught the head and implement the main logic:
         while(head != NULL)
         {
-            if(head -> val < x)
-            {
-                //small list:
+            if(head-> val < x){
                 smallHead->next = head;
                 smallHead = smallHead->next;
             }else{
-                //high list:
                 highHead->next = head;
                 highHead = highHead->next;
             }
             head = head->next;
         }
+        // step_03: Build connection between small and high LIstNOde:
         highHead->next = NULL;
-        smallHead->next = higher->next;
+        smallHead->next = high->next;
         
         return small->next;
     }
