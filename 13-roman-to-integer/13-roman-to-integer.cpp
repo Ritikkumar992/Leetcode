50cf831b43c5b1d7597e268571b237d8ddd5971c
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-07-28 11:34:57 +0530

    Time: 22 ms (40.63%), Space: 7.8 MB (48.91%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        //Approach: Using Map: Time:O(nlogn) and Space:O(n)
         unordered_map<char,int> mp = 
         {
             {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
