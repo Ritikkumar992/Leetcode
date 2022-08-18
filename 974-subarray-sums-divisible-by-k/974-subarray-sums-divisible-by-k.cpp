37af593bc809e51ac14097cbf7165838d41bd1f6
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-08-18 16:14:40 +0530

    Time: 135 ms (15.58%), Space: 32.1 MB (10.28%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -3,20 +3,17 @@ public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n = nums.size();
         int ans = 0, sum = 0, rem = 0;
-        map<int,int>mp;
+        map<int,int> mp;
         mp[0] = 1;
         
         for(int i = 0;i<n;i++)
         {
             sum += nums[i];
             rem = sum%k;
-            
             if(rem < 0)
                 rem += k;
-            // we have to store the frequency of rem in our mp:
             if(mp.find(rem) != mp.end())
                 ans += mp[rem];
-            // increase the frequency of our rem in map;
             mp[rem]++;
         }
         return ans;
