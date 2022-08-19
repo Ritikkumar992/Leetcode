b594de298bd9b5df5e590051cd8c21b168ab1da1
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-08-19 11:06:07 +0530

    Time: 108 ms (56.46%), Space: 48.4 MB (78.24%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        //Observation Based:
         int n = nums.size();
         int reachable = 0;
         
-        for(int i= 0;i<n;i++){
+        for(int i = 0;i<n;i++){
             if(reachable < i)
                 return false;
             reachable = max(reachable, i+nums[i]);
