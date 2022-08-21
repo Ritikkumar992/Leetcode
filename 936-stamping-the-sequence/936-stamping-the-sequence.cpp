06f0d81e4ea77017dd96e2c962ce9fc7c829c8c0
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-08-21 11:38:07 +0530

    Time: 24 ms (58.20%), Space: 7.5 MB (76.23%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+
+public:
+    vector<int> movesToStamp(string s, string t) {
+        int n = t.size(), m = s.size();
+        
+        auto check = [&](int i) {
+            for (int j = 0; j < m; j++) {
+                if (t[i + j] != '?' && t[i + j] != s[j]) return false;
+            }
+            for (int j = 0; j < m; j++) {
+                if (t[i + j] != '?') return true;
+            }
+            return false;
+        };
+        
+        vector<int> moves;
+        string done(n, '?');
+        
+        while (t != done) {
+            bool move = false;
+            for (int i = 0; i < n - m + 1; i++) {
+                if (check(i)) {
+                    fill(t.begin() + i, t.begin() + i + m, '?');
+                    moves.push_back(i);
+                    move = true;
+                }
+            }
+            if (!move) return {};
+        }
+        
+        reverse(moves.begin(), moves.end());
+        return moves;
+    }
+};
