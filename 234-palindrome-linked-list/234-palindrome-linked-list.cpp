08e8f00e0e5cf502066c9cba74262d88da05726c
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-08-23 07:33:48 +0530

    Time: 392 ms (44.20%), Space: 123 MB (30.59%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -9,48 +9,18 @@
  * };
  */
 class Solution {
-    // function to reverse the linked list:
-    ListNode* reverse(ListNode* ptr) {
-    ListNode* pre=NULL;
-    ListNode* nex=NULL;
-    while(ptr!=NULL) {
-        nex = ptr->next;
-        ptr->next = pre;
-        pre=ptr;
-        ptr=nex;
-    }
-    return pre;
-}
-    
 public:
     bool isPalindrome(ListNode* head) {
-        //Without using Extra Space:
-        //TIme;O(N/2)+O(N/2)+O(N/2) and Space: O(1)
-        
-        //Step 1: Getting the middle element of the list;
-        ListNode* fast = head;
-        ListNode* slow = head;
-        
-        while(fast->next != NULL && fast->next->next != NULL)
-        {
-            fast = fast->next->next;
-            slow = slow->next;
+        //Approach_01:convert the linked list to string and check for palindorme:
+        string s = "";
+        while(head != NULL){
+            s += head->val;
+            head = head->next;
         }
-        //Traversing the list from the next element of the slow pointer.
-        slow->next = reverse(slow->next);
-        
-        //Step3: iterate throught the list until the dummy doesn't reaches end, and 
-        // create a dummy node to check if they are same element or not.
-        slow = slow->next;
-        ListNode* dummy = head;
-        
-        while(slow != NULL)
-        {
-            if(slow->val != dummy->val)
-                return false;
-            slow = slow->next;
-            dummy = dummy->next;
+        string s1 = "";
+        for(int i = s.size()-1;i>=0;i--){
+            s1 += s[i];
         }
-        return true;
+        return s == s1;
     }
 };
