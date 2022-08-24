ec22500001a5339bb58530a8e286ef9a9ea42f3a
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-08-24 09:13:00 +0530

    Time: 43 ms (28.42%), Space: 5.9 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        //Approach_01:Using recursion
-        if(n == 1)
-            return true;
-        if(n == 0)
-            return false;
-        if(n%3 == 0){
-            return isPowerOfThree(n/3);
-        }
-        return false;
+        // 1162261467 is 3^19,  3^20 is bigger than int  
+        return ( n>0 &&  1162261467%n==0);
     }
 };
