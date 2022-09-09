123b650a7e54541c42086ddd6a79e8365712c9a6
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-09-09 13:18:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.34%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        // Time:O(n^2) and Space:O(n^2)
         vector<vector<int>> ans(numRows);
-        for(int i=0;i<numRows;i++){
+        for(int i = 0;i<numRows;i++){
             ans[i].resize(i+1);
             ans[i][0] = ans[i][i] = 1;
-            
             for(int j = 1;j<i;j++){
                 ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
             }
