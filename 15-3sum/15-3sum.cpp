146d320d48dea674902eb6106f73964308d25d32
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-09-20 12:05:43 +0530

    Time: 147 ms (56.31%), Space: 21.1 MB (46.02%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        //Approach_02: Two Pointer + 2 sum:
-        vector<vector<int>> res;
-        sort(nums.begin(),nums.end());
+    vector<vector<int> > threeSum(vector<int> &num) {
+    
+    vector<vector<int> > res;
+
+    std::sort(num.begin(), num.end());
+
+    for (int i = 0; i < num.size(); i++) {
         
-        int n = nums.size();
-        for(int i = 0;i<n-2;i++)
-        {
-            if(i == 0 || (i>0 && nums[i] != nums[i-1]))
-            {
-                int lo = i+1, hi = n-1, sum = 0 - nums[i];
-                while(lo<hi)
-                {
-                    if(nums[lo]+nums[hi] == sum){
-                        vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[lo]);
-                        temp.push_back(nums[hi]);
-                        res.push_back(temp);
-                        
-                        // to avoid the duplicate element:
-                        while(lo<hi && nums[lo] == nums[lo+1])lo++;
-                        while(lo<hi && nums[hi] == nums[hi-1])hi--;
-                        
-                        lo++;
-                        hi--;
-                    }
-                    else if(nums[lo]+nums[hi] > sum)
-                        hi--;
-                    else
-                        lo++;
-                }
+        int target = -num[i];
+        int front = i + 1;
+        int back = num.size() - 1;
+
+        while (front < back) {
+
+            int sum = num[front] + num[back];
+            
+            // Finding answer which start from number num[i]
+            if (sum < target)
+                front++;
+
+            else if (sum > target)
+                back--;
+
+            else {
+                vector<int> triplet = {num[i], num[front], num[back]};
+                res.push_back(triplet);
+                
+                // Processing duplicates of Number 2
+                // Rolling the front pointer to the next different number forwards
+                while (front < back && num[front] == triplet[1]) front++;
+
+                // Processing duplicates of Number 3
+                // Rolling the back pointer to the next different number backwards
+                while (front < back && num[back] == triplet[2]) back--;
             }
+            
         }
-        return res;
+
+        // Processing duplicates of Number 1
+        while (i + 1 < num.size() && num[i + 1] == num[i]) 
+            i++;
+
     }
+    
+    return res;
+    
+}
 };
