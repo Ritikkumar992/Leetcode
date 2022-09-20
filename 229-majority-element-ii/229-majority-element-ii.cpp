e47c1cbb2a3e72b85d150dd53e28e34cd9c3c8c1
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-09-20 11:19:04 +0530

    Time: 21 ms (58.45%), Space: 16 MB (17.12%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-         //Optimal Solution (Extended Boyer Moore’s Voting Algorithm)
-        // Time : O(n) and Space : O(1)
-        int sz = nums.size();
-        int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
-          for (i = 0; i < sz; i++) {
-                if (nums[i] == num1)
-                  count1++;
-                else if (nums[i] == num2)
-                  count2++;
-                else if (count1 == 0) {
-                  num1 = nums[i];
-                  count1 = 1;
-                } else if (count2 == 0) {
-                  num2 = nums[i];
-                  count2 = 1;
-                } else {
-                  count1--;
-                  count2--;
-                }
-          }
-          vector < int > ans;
-          count1 = count2 = 0;
-              for (i = 0; i < sz; i++) {
-                if (nums[i] == num1)
-                  count1++;
-                else if (nums[i] == num2)
-                  count2++;
-              }
-              if (count1 > sz / 3)
-                ans.push_back(num1);
-              if (count2 > sz / 3)
-                ans.push_back(num2);
-              return ans;
-            }
+        vector<int> ans;
+        map<int,int> mp;
+        for(auto num:nums){
+            mp[num]++;
+        }
+        for(auto m:mp){
+            if(m.second>nums.size()/3)
+                ans.push_back(m.first);
+        }
+        return ans;
+    }
 };
