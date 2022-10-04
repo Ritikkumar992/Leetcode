9a1d54220346efc7ba16718124e4c8aba246bd07
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2022-10-04 15:55:20 +0530

    Time: 49 ms (31.23%), Space: 16.9 MB (86.25%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //usign xor:
         int ans = 0;
-        for(auto x:nums){
-            ans^=x;
+        for(auto n:nums){
+            ans^=n;
         }
         return ans;
     }
