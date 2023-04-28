6557ec5b37e4940a668cfc05066f5084684ea346
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-04-28 15:20:44 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    int solveUtil(int ind, vector<int>& height, vector<int>& dp, int k){
-    if(ind==0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
-    
-    int mmSteps = INT_MAX;
+    int solveUtil(int n, vector<int>& height, vector<int>& dp, int k){
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        int mmSteps = INT_MAX;
         
-    for(int j=1;j<=k;j++){
-        if(ind-j>=0){
-      int jump = solveUtil(ind-j, height, dp, k)+ abs(height[ind]- height[ind-j]);
-            mmSteps= min(jump, mmSteps);
+        for(int j=1;j<=k;j++){
+            if(i-j>=0){
+                int jump = dp[i-j]+ abs(height[i]- height[i-j]);
+                mmSteps= min(jump, mmSteps);
+            }
         }
+        dp[i]= mmSteps;
     }
-    return dp[ind]= mmSteps;
-    
+    return dp[n-1];
 }
 
 int solve(int n, vector<int>& height , int k){
     vector<int> dp(n,-1);
-    return solveUtil(n-1, height, dp, k);
+    return solveUtil(n, height, dp, k);
 }
   public:
     int minimizeCost(vector<int>& height, int n, int k) {
