bc6f7625f0fb8072a82304d509807ba976ccd8ff
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-04-28 12:08:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // recursive appraoch to tabulation => time: O(n) and space: O(n)
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
-        
+        // most optimized approach => time: O(n) and space: O(1)
+        int prev2 = 1, prev = 1;
         for(int i = 2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int cur_i = prev2+prev;
+            prev2 = prev;
+            prev = cur_i;
         }
-        return dp[n];
+        return prev;
     }
 };
