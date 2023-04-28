8aa595dc6ab50cd4b0d090a5a0e92a39a5851dc6
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-04-28 11:25:34 +0530

    Time: 3 ms (70.49%), Space: 6.1 MB (21.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
+    int helper(int n, vector<int>&dp){
+        // base case:
+        if(n<=1)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = helper(n-1, dp) + helper(n-2,dp);
+    }
 public:
     int fib(int n) {
-        // recursive apporach : time: O(log n)
+        // memoization : time: O(n) and space:O(n) + O(n)
+        vector<int> dp(n+1, -1);
+        int ans = helper(n , dp);
         
-        // base case:
-        if(n<=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2); // main logic.
+        return ans;
     }
 };
