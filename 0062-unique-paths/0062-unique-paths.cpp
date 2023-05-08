171ef5b85855bb1705682d6439e2547c7ec99e1d
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-05-08 14:56:35 +0530

    Time: 4 ms (24.23%), Space: 6.2 MB (69.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,32 @@
 class Solution {
-    int countWaysUtil(int m, int n, vector<vector<int> > &dp) {
-  for(int i=0; i<m ;i++){
-      for(int j=0; j<n; j++){
-          
-          //base condition
-          if(i==0 && j==0){
-              dp[i][j]=1;
-              continue;
-          }
-          
-          int up=0;
-          int left = 0;
-          
-          if(i>0) 
-            up = dp[i-1][j];
-          if(j>0)
-            left = dp[i][j-1];
-            
-          dp[i][j] = up+left;
-      }
-  }
-  
-  return dp[m-1][n-1];
-
-  
-}
 public:
+    int countWays(int m, int n){
+    vector<int> prev(n,0);
+    for(int i=0; i<m; i++){
+        vector<int> temp(n,0);
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0){
+                temp[j]=1;
+                continue;
+            }
+            
+            int up=0;
+            int left =0;
+            
+            if(i>0)
+                up = prev[j];
+            if(j>0)
+                left = temp[j-1];
+                
+            temp[j] = up + left;
+        }
+        prev = temp;
+    }
+    
+    return prev[n-1];
+    
+}
     int uniquePaths(int m, int n) {
-         //  : time:Tabulation O(n*m) and space: O(n*m)
-    vector<vector<int> > dp(m,vector<int>(n,-1));
-    return countWaysUtil(m,n,dp);
+        return countWays(m,n);
     }
 };
