920fc2e8d0e49ae3914ea61e1160d2520e0804b0
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-05-11 10:53:26 +0530

    Time: 16 ms (57.25%), Space: 10.2 MB (58.67%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& A) {
-  for (auto i = 1; i < A.size(); ++i)
-    for (auto j = 0; j < A.size(); ++j)
-      A[i][j] += min({ A[i-1][j], A[i-1][max(0,j-1)], A[i-1][min((int)A.size()-1,j+1)] });
-  return *min_element(begin(A[A.size() - 1]), end(A[A.size() - 1]));
-}
+    int solve(int i, int j, int m, vector<vector<int>>&matrix, vector<vector<int>>&dp)
+    {
+        // base case:
+        if(j<0 || j>=m)
+            return 1e9;
+        if(i == 0)
+            return matrix[0][j];
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = matrix[i][j] + solve(i-1,j,m,matrix ,dp);
+        int leftDiagonal = matrix[i][j] + solve(i-1,j-1,m,matrix,dp);
+        int rightDiagonal = matrix[i][j] + solve(i-1,j+1,m,matrix,dp);
+        
+        return dp[i][j] =  min(up, min(leftDiagonal, rightDiagonal));
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // Recusive Approach: time: exponential and space:O(n)
+        //   row                   col
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        
+        int minm = 1e9;
+        for(int j = 0;j<m;j++)
+        {
+            int ans = solve(n-1,j,m,matrix, dp);
+            minm = min(ans, minm);
+        }
+        return minm;
+    }
 };
