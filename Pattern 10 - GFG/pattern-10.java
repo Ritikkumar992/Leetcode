5775ac6dffd960fa7605b08db6590fbf074ed46e
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-06-22 13:31:23 +0530

    Added solution - LeetHub

--- Pattern 10 - GFG/pattern-10.java
+++ Pattern 10 - GFG/pattern-10.java
@@ -21,19 +21,21 @@ class Main {
 class Solution {
 
     void printTriangle(int n) {
-        // code here
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
         
-        for(int i = n-1;i>=1;i--){
+        for(int i = 1;i<=n;i++)
+        {
             for(int j = 1;j<=i;j++){
                 System.out.print("* ");
             }
-            System.out.println("");
+            System.out.println();
+        }
+        
+        for(int i = n-1;i>=1;i--)
+        {
+            for(int j = i;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
