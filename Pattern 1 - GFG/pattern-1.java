c43226151bba400f8ab20ea5dbc5bf0903704b5d
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-06-22 11:57:35 +0530

    Added solution - LeetHub

--- Pattern 1 - GFG/pattern-1.java
+++ Pattern 1 - GFG/pattern-1.java
@@ -22,11 +22,11 @@ class Solution {
 
     void printSquare(int n) {
         // code here
-        for(int i = 0;i<n;i++){
+        for(int i= 0;i<n;i++){
             for(int j = 0;j<n;j++){
                 System.out.print("* ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
