2af291141a82364fffa7bb5cea65afc2d0800062
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-06-23 11:09:36 +0530

    Added solution - LeetHub

--- Pattern 13 - GFG/pattern-13.java
+++ Pattern 13 - GFG/pattern-13.java
@@ -24,12 +24,14 @@ class Solution {
 
     void printTriangle(int n) {
         int cnt = 1;
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=i;j++){
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=i;j++)
+            {
                 System.out.print(cnt+" ");
                 cnt++;
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
