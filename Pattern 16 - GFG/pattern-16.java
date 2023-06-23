0e3a555e6c669c4d9ec5c3b760ea92b5b6a87666
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-06-23 11:32:35 +0530

    Added solution - LeetHub

--- Pattern 16 - GFG/pattern-16.java
+++ Pattern 16 - GFG/pattern-16.java
@@ -21,14 +21,16 @@ class Main {
 class Solution {
 
     void printTriangle(int n) {
-        // code here
         char c = 'A';
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=i;j++){
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=i;j++)
+            {
                 System.out.print(c);
             }
             c++;
-            System.out.println("");
+            System.out.println();
         }
+        
     }
 }
