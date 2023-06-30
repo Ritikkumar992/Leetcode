27d8c5e256bacb777ab1ab5cd52e84d234391c4e
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-06-30 15:51:30 +0530

    Time: 29 ms (31.72%), Space: 16.7 MB (99.18%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& A) {
         int N = A.size();
-        map<int,int>mp;
-	    for(int i =0;i<N;i++){
-	        mp[A[i]]++;
+        int xorr = 0;
+	    for(int i = 0;i<N;i++)
+	    {
+	        xorr = xorr^A[i];
 	    }
-	    for(auto m:mp){
-	        if(m.second == 1){
-	            return m.first;
-	        }
-	    }
-	    return -1;
+	    return xorr;
     }
 };
