d5c032b7db8a2752275283c31d6dd47ee8cb5ee7
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-07-03 21:26:33 +0530

    Time: 32 ms (44.69%), Space: 22.1 MB (99.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //Approach_04: Binary Search : Time:O(logn) and Space:O(1)
-        int n = nums.size();
         int low = 0;
-        int high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (mid % 2 == 0) {
-                if (nums[mid] != nums[mid + 1]) 
-                    //Checking whether we are in right half
-                    high = mid - 1; //Shrinking the right half
-                    else
-                        low = mid + 1; //Shrinking the left half
-                } else {
-                    //Checking whether we are in right half
-                    if (nums[mid] == nums[mid + 1]) 
-                        high = mid - 1; //Shrinking the right half
-                    else
-                        low = mid + 1; //Shrinking the left half
-                }
-            }
-            return nums[low];
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if mid is the start of a pair
+            if (nums[mid] == nums[mid ^ 1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return nums[low];
     }
 };
