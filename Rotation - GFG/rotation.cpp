39e12f90e972bf241b6feaf075280750a09c9eb8
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-07-03 16:00:37 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -9,17 +9,47 @@ using namespace std;
 class Solution{
 public:	
 	int findKRotation(int arr[], int n) {
-	   int mn = INT_MAX;
-	   int ans = -1;
-	   for(int i = 0;i<n;i++)
-	   {
-	       if(arr[i] < mn){
-	           mn = min(arr[i], mn);
-	           ans = i;
-	       }
-	   }
-	   return ans;
-	}
+    int low = 0, high = n - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            if (arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
+}
 
 };
 
