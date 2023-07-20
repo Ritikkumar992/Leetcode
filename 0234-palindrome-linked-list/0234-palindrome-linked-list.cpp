45d35f1b731f47529a774905d49db9f5eff8111a
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-07-20 10:44:35 +0530

    Time: 275 ms (42.53%), Space: 117.9 MB (64.35%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,20 +10,44 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        // Brute force apprach:
-        vector<int> ans;
-        while(head != NULL){
-            ans.push_back(head->val);
-            head = head->next;
-        }
-        int i = 0, j = ans.size()-1;
-        while(i<j)
+    ListNode* reverse(ListNode* node)
+    {
+        ListNode* prev = NULL;
+        ListNode* curr = node;
+        ListNode* front;
+        
+        while(curr != NULL)
         {
-            if(ans[i] != ans[j])
+            front = curr->next;
+            curr->next = prev;
+            
+            prev = curr;
+            curr = front;
+        }
+        node = prev;
+        return node;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        // optmized approach:
+        ListNode* slow = head, *fast = head;
+        
+        // step_01: finding middle node.
+        while(fast -> next != NULL && fast->next->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        // step_02: Reversing the node from the next node of the slow pointer.
+        slow->next = reverse(slow->next);
+        slow = slow->next;
+        
+        // step_03: check for palindrome .
+        ListNode* dummy = head;
+        while(slow != NULL) {
+            if(dummy->val != slow->val) 
                 return false;
-            i++;
-            j--;
+            dummy = dummy->next;
+            slow = slow->next;
         }
         return true;
     }
