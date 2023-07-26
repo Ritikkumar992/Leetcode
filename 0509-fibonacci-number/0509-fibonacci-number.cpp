0fb83057ad05cdd6a88d5d0e6015c348cadbbad7
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-07-26 16:01:05 +0530

    Time: 14 ms (39.97%), Space: 5.9 MB (91.50%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,9 @@
 class Solution {
-    int helper(int n, vector<int>&dp){
-        // base case:
-        if(n<=1)
-            return n;
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = helper(n-1, dp) + helper(n-2,dp);
-    }
 public:
     int fib(int n) {
-        // memoization : time: O(n) and space:O(n) + O(n)
-        vector<int> dp(n+1, -1);
-        int ans = helper(n , dp);
-        
-        return ans;
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 };
