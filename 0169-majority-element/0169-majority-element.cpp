32bf6342b07ae24bdf84561875b6b50a30f28202
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-08-02 17:11:15 +0530

    Time: 12 ms (94.35%), Space: 19.7 MB (12.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& v) {
         // //Brute force:
         // for(int i =0;i<nums.size();i++)
         // {
@@ -17,15 +17,38 @@ public:
         // return -1;
         
         // using map:
-        unordered_map<int,int> mp;
-        for(auto num:nums){
-            mp[num]++;
-        }
-        for(auto m:mp){
-            if(m.second> nums.size()/2){
-                return m.first;
+        // unordered_map<int,int> mp;
+        // for(auto num:nums){
+        //     mp[num]++;
+        // }
+        // for(auto m:mp){
+        //     if(m.second> nums.size()/2){
+        //         return m.first;
+        //     }
+        // }
+        // return -1;
+        
+        // Approach_03: 
+        int n = v.size(), cnt = 0, ele;
+        for(int i = 0;i<n;i++){
+            if(cnt == 0){
+                cnt = 1;
+                ele = v[i];
+            }
+            else if(ele == v[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
+        int cnt1 = 0;
+        for(int i = 0;i<n;i++){
+            if(v[i] == ele){
+                cnt1++;
+            }
+        }
+        if (cnt1 > (n / 2)) return ele;
         return -1;
     }
 };
