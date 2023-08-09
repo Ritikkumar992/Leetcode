2e63f15f19d3ed3ce152c6a85dd72ba3c39973df
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-08-09 11:33:42 +0530

    Time: 120 ms (98.99%), Space: 81.2 MB (38.62%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isPossible(vector<int>&nums, int p, int diff)
     {
-        int n = nums.size();
+        int  n = nums.size();
         for(int i = 1;i<n;i++)
         {
-            if(nums[i] - nums[i-1] <= diff){
+            if(nums[i]-nums[i-1] <= diff){
                 p--;
                 i++;
             }
@@ -18,11 +18,11 @@ public:
         int n = nums.size();
         int left = 0, right = nums[n-1]-nums[0];
         int ans = right;
+        
         while(left <= right)
         {
             int mid = left + (right-left)/2;
-            if(isPossible(nums, p, mid))
-            {
+            if(isPossible(nums, p, mid)){
                 ans = mid;
                 right = mid-1;
             }
