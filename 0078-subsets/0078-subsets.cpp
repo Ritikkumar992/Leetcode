a4aed6fc33d94c8410198643d6aaa599c82dfae9
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-08-14 23:16:51 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (40.22%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        ans.push_back({});
-        
-        for(int num:nums) // 1 2 3
-        {
-            int n = ans.size();
-            for(int j = 0;j<n;j++){
-                // step_01: copy list:
-                vector<int> temp = ans[j];
-                temp.push_back(num);
-                ans.push_back(temp);
-            }
+    vector<vector<int>> allSubsets;
+    void generate(vector<int>&subset, int i, vector<int>&nums)
+    {
+        if(i == nums.size()){
+            allSubsets.push_back(subset);
+            return;
         }
+        // ith element is not in subset.
+        generate(subset, i+1, nums);
         
-        return ans;
+        // ith element is in subset.
+        subset.push_back(nums[i]);
+        generate(subset, i+1, nums);
+        subset.pop_back();
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> empty;
+        generate(empty, 0, nums);
+        return allSubsets;
     }
 };
