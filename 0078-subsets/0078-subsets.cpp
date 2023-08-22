c07be8acc7e196cf964eb4c3ecbd6448e778f23e
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-08-22 15:57:26 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (46.19%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> allSubsets;
-    void generate(vector<int>&subset, int i, vector<int>&nums)
+    void solve(int ind, vector<int>& nums, vector<int>& temp,vector<vector<int>>&ans)
     {
-        if(i == nums.size()){
-            allSubsets.push_back(subset);
+        if(ind == nums.size()){
+            ans.push_back(temp);
             return;
         }
-        // ith element is not in subset.
-        generate(subset, i+1, nums);
-        
-        // ith element is in subset.
-        subset.push_back(nums[i]);
-        generate(subset, i+1, nums);
-        subset.pop_back();
+        // not pick:
+        solve(ind+1, nums, temp, ans);
+        // pick:
+        temp.push_back(nums[ind]);
+        solve(ind+1, nums, temp, ans);
+        temp.pop_back();
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> empty;
-        generate(empty, 0, nums);
-        return allSubsets;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0, nums, temp, ans);
+        return ans;
     }
 };
