db2ace5d2fc461818d4070d55f7b05f912972f3a
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-08-26 15:52:04 +0530

    Time: 65 ms (83.87%), Space: 24.5 MB (35.80%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    static bool comp(const vector<int>&a, const vector<int>&b){
-        if(a[1]==b[1]) return a[0]<b[0];
-        return a[1]<b[1];
+    static bool compare(const vector<int>&a, const vector<int>&b)
+    {
+        if(a[1] == b[1])
+            return a[0] < b[0];
+        return a[1] < b[1];
     }
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end(), comp);
+        sort(pairs.begin(), pairs.end(), compare);
+        
         int mx = INT_MIN, ans = 0;
         
-        for(auto pair: pairs)
+        for(auto pair:pairs)
         {
             if(pair[0] > mx){
                 ans++;
