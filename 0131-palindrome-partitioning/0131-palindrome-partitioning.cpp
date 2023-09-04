3ace300b27761e107582487f1d2ca2f85b9fd912
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-09-04 13:05:25 +0530

    Time: 92 ms (82.83%), Space: 53.3 MB (75.74%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+    bool isPalindrome(string s, int start, int end){
+        while(start <= end){
+            if(s[start] != s[end])
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    void solve(int index, string &s, vector<string>& res, vector<vector<string>>& ans){
+        if(index == s.size()){
+            ans.push_back(res);
+            return;
+        }
+        for(int i = index;i<s.size();i++){
+            if(isPalindrome(s,index, i)){
+                res.push_back(s.substr(index, i-index+1));
+                solve(i+1, s, res, ans);
+                res.pop_back();
+            }
+        }
+    }
+public:
     vector<vector<string>> partition(string s) {
-      vector<vector<string>>res;
-      vector<string>path;
-      func(0, s, path, res);
-      return res;
+        vector<vector<string>> ans;
+        vector<string> res;
+        solve(0, s, res, ans);
+        return ans;
     }
-void func(int index, string s, vector<string> & path,
-    vector<vector<string>> & res) {
-    if (index == s.size()) {
-      res.push_back(path);
-      return;
-    }
-    for (int i = index; i<s.size(); ++i) {
-      if (isPalindrome(s, index, i)) {
-        path.push_back(s.substr(index, i - index + 1));
-        func(i + 1, s, path, res);
-        path.pop_back();
-      }
-    }
-  }
-  bool isPalindrome(string s, int start, int end) {
-    while (start <= end) {
-      if (s[start++] != s[end--])
-        return false;
-    }
-    return true;
-  }
 };
