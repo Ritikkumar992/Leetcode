4f49a3bd3e556a032f4b680e46db7818d39f294f
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2023-09-04 14:31:38 +0530

    Time: 818 ms (38.93%), Space: 8.1 MB (44.27%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,23 +1,43 @@
 class Solution {
-    bool search(vector<vector<char>>& board, int i, int j, string& word, int idx){
-        if(idx == word.length() - 1) return true;
-        board[i][j] -= 65;
-        if(i > 0 && board[i-1][j] == word[idx+1] && search(board, i-1, j, word, idx+1)) return true;
-        if(j > 0 && board[i][j-1] == word[idx+1] && search(board, i, j-1, word, idx+1)) return true;
-        if(i < board.size()-1 && board[i+1][j] == word[idx+1] && search(board, i+1, j, word, idx+1)) return true;
-        if(j < board[0].size()-1 && board[i][j+1] == word[idx+1] && search(board, i, j+1, word, idx+1)) return true;
-        board[i][j] += 65;
-        return false;
-}
-
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int r = board.size();
-        if(r == 0) return false;
-        int c = board[0].size();
-        for(int i = 0; i < r; ++i)
-            for(int j = 0; j < c; ++j)
-                if(board[i][j] == word[0] && search(board, i, j, word, 0)) return true;
+    bool searchNext(vector<vector<char>> &board, string word, int row, int col, 
+    int index, int m, int n) {
+
+        if (index == word.length())
+            return true;
+
+        if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word[index] or board[row][col] == '!')
+            return false;
+
+        char c = board[row][col];
+        board[row][col] = '!';
+
+        bool top = searchNext(board, word, row - 1, col, index + 1, m, n);
+        bool right = searchNext(board, word, row, col + 1, index + 1, m, n);
+        bool bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
+        bool left = searchNext(board, word, row, col - 1, index + 1, m, n);
+
+        board[row][col] = c; // undo change
+
+        return top || right || bottom || left;
+    }
+    bool exist(vector<vector<char>> board, string word) {
+
+        int m = board.size();
+        int n = board[0].size();
+
+        int index = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] == word[index]) {
+                    if (searchNext(board, word, i, j, index, m, n))
+                        return true;
+                }
+            }
+        }
+
         return false;
     }
 };
